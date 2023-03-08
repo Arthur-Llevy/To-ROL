a5eb64e86a5c2b03f61a1706426af87941351da4
Arthur Levy <arthurlbvieiraa@gmail.com>
2023-03-07 21:32:45 -0300

    Adicionado responsividade.

--- main.js
+++ main.js
@@ -23,12 +23,13 @@ orderItems.addEventListener('click', () => {
         let li = document.createElement('li');
 
         li.innerHTML = list[i];
-        
-
-        
+                
         if(i <= list.length){
             
             let li = document.createElement('li');
+            let h2 = document.getElementById('h2');
+
+            h2.innerHTML = 'A ordem dos números digitados é'
             li.innerHTML = list[i];
             ul.appendChild(li);
             li.setAttribute('id',  i)
