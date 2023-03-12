c34897b837370d92aa0f32bc270cdc4c661e5a3b
Arthur Levy <arthurlbvieiraa@gmail.com>
2023-03-11 22:02:20 -0300

    Adiconada parte 3 da atualização,

--- main.js
+++ main.js
@@ -19,15 +19,19 @@ createClass.addEventListener('click', () => {
     let thirdInput = document.createElement('input');
     
     fieldset.setAttribute('id', `class${classNumber}`)
+    fieldset.setAttribute('class', `classes`)
     legend.innerHTML = `Dados - Classe ${classNumber}`;
     firstP.innerHTML = `Nome da classe`;
     secondP.innerHTML = 'De';
     firstInput.setAttribute('id', 'class-name');
+    firstInput.setAttribute('class', 'class-names');
     thirdP.innerHTML = 'Até';
     secondInput.setAttribute('id', 'to');
     secondInput.setAttribute('type', 'number');
+    secondInput.setAttribute('class', 'froms');
     thirdInput.setAttribute('id', 'to');
     thirdInput.setAttribute('type', 'number');
+    thirdInput.setAttribute('class', 'tos');
 
     fieldset.appendChild(legend);
     fieldset.appendChild(firstP);
@@ -78,17 +82,38 @@ addItem.addEventListener('click', () => {
 });
 
 let orderItems = document.getElementById('toOrder');
+let classValues = [];
 
-orderItems.addEventListener('click', () => {   
+orderItems.addEventListener('click', () => {         
 
     list.sort((a, b) => a - b);
     
     let table = document.getElementById('table-datas');
+    let tableStatistics = document.getElementById('table-datas-statistics');
     table.style.visibility = 'initial';
+    tableStatistics.style.visibility = 'initial';
 
+    let tableStatisticsFirstLine = document.createElement('tr');
+    let tableStatisticsClassName = document.createElement('th');
+    let tableStatisticsFa = document.createElement('th');
+    let tableStatisticsPercent = document.createElement('th');
+
+    let tableStatisticsFe = document.createElement('th');
     let tableFirstLine = document.createElement('tr');
     let tableOrderLine = document.createElement('th');
     let tableValueLine = document.createElement('th');
+
+    tableStatisticsClassName.innerHTML = 'Classe';
+    tableStatisticsFa.innerHTML = 'Fr. absoluta';
+    tableStatisticsFe.innerHTML = 'Fr. relativa ';
+    tableStatisticsPercent.innerHTML = '%';
+
+
+    tableStatisticsFirstLine.appendChild(tableStatisticsClassName);
+    tableStatisticsFirstLine.appendChild(tableStatisticsFa);
+    tableStatisticsFirstLine.appendChild(tableStatisticsFe);
+    tableStatisticsFirstLine.appendChild(tableStatisticsPercent);
+    tableStatistics.appendChild(tableStatisticsFirstLine);
 
     tableOrderLine.innerHTML = 'Ordem';
     tableValueLine.innerHTML = 'Valores';
@@ -114,7 +139,44 @@ orderItems.addEventListener('click', () => {
 
     };
 
+    let classes = document.querySelectorAll('.classes');
+    let classesNames = document.querySelectorAll('.class-names');
+    let classesTos = document.querySelectorAll('.tos');
+    let classesFroms = document.querySelectorAll('.froms');
+
+    for(let i = 0; i <= classes.length; i++){
+
+        let statisticsTr = document.createElement('tr');
+        let classThName = document.createElement('th');
+        let classThFa = document.createElement('th');
+        let classThFe = document.createElement('th');
+        let classThPercentage = document.createElement('th');
+
+        classThName.innerHTML = classesNames[i]?.value
+        statisticsTr.appendChild(classThName);
+        tableStatistics.appendChild(statisticsTr)
+
+
+    };
+
+    let tableStatisticsTotal = document.createElement('td');
+    let tableStatisticsTotalFa = document.createElement('td');
+    let tableStatisticsTotalFr = document.createElement('td');
+    let tableStatisticsTotalPercent = document.createElement('td');
+
+    for (let i = 0; i <= list.length; i++ ){
+
+        if(list[i] >= classesFroms[classNumber - 1].value){
+            console.log(classesTos[i].value)
+            classValues.push(list[i])
+            // console.log(classValues)
+
+        };
+
+    };   
+
     list = [];    
+
 
 });
 
@@ -128,6 +190,7 @@ resetButton.addEventListener('click', () => {
     while (table.firstChild) { 
         table.removeChild(table.firstChild); 
     };
+ 
 
 });
 
